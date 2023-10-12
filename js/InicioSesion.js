b4b286702aec51856da36fa890be5386af904227
MatiasFrassia <mfrassia27@gmail.com>
2023-10-12 09:59:11 -0300

    Se agrego el objeto administrador

---
+++ js/InicioSesion.js
@@ -0,0 +1,9 @@
+const administrador = {
+    nombre: "Matias",
+    apellido: "Frassia",
+    username: "Administrador",
+    contraseña: "123456",
+    email: "mfrassia27@gmail.com",
+    fechaNacimiento: "27/06/2000"
+}
+
