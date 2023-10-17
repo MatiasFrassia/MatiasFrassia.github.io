0ef9a92425e5439417cb57b65b2985f6d8be67db
MatiasFrassia <mfrassia27@gmail.com>
2023-10-17 17:26:02 -0300

    actualizacion

--- js/InicioSesion.js
+++ js/InicioSesion.js
@@ -1,9 +1,124 @@
-const administrador = {
-    nombre: "Matias",
-    apellido: "Frassia",
-    username: "Administrador",
-    contraseña: "123456",
-    email: "mfrassia27@gmail.com",
-    fechaNacimiento: "27/06/2000"
-}
+document.addEventListener("DOMContentLoaded", function() {
+   class Usuario{
+        constructor(nombre, apellido, username, contraseña, email, fechaNacimiento){
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.username = username;
+            this.contraseña = contraseña;
+            this.email = email;
+            this.fechaNacimiento = fechaNacimiento;
+        }
+    }
+    class Administrador extends Usuario{
+        constructor(nombre, apellido, username, contraseña, email, fechaNacimiento){
+            super(nombre, apellido, username, contraseña, email, fechaNacimiento);
+        }
+        static crearAdministrador(){
+                alert("Ingrese los datos del usuario que desea volver administrador");
+                const nombre = prompt("Nombre:");
+                const apellido = prompt("Apellido:");
+                const username = prompt("Username");
+                const contraseña = prompt("Contraseña:");
+                const email = prompt("Email");
+                const fechaNacimiento = prompt("Fecha de Nacimiento");
+                const nuevoAdministrador = new Administrador(nombre, apellido, username, contraseña, email, fechaNacimiento);
+                administradores.push(nuevoAdministrador);
+        }
+    }
+    let administradores = [];
+    let usuarios = [];
+    const administradorOriginal = new Administrador("Matias", "Frassia", "Nybras", "12345", "mfrassia27@gmail.com", "27/06/2000"); 
+    administradores.push(administradorOriginal);
+    usuarios.push(administradorOriginal);
+
+    //LOGIN
+    function initializeLoginPage() {
+        document.getElementById("loginForm").addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            const emailLogin = document.getElementById("emailLogin").value;
+            const passwordLogin = document.getElementById("passwordLogin").value;
+
+            const userDataLogin = {
+                email: emailLogin,
+                contraseña: passwordLogin
+            };
+            document.getElementById("loginForm").reset();
+            let loginAccess = false;
+            for(let usuario of usuarios){
+                if((usuario.email == userDataLogin.email) && (usuario.contraseña == userDataLogin.contraseña)){
+                    loginAccess = true;
+                    alert(`   Bienvenido ${usuario.username}. 
+                    Toma asiento y disfruta tu estadia.`)
+                    for(let administrador of administradores){
+                        if(administrador.email == userDataLogin.email){
+                            alert("Usted posee acceso admnistrador");
+                            let respuesta = prompt("Desea crear un nuevo administrador?");
+                            if(respuesta == "Si"){
+                                Administrador.crearAdministrador();
+                            }
+                        }
+                        break;
+                    }
+                    break;
+                }
+            }
+            if(loginAccess == false){
+                alert(`Usuario y/o contraseña incorrecta`)
+            }
+
+        });
+    }
+
+    //REGISTRO
+    function initializeRegistrationPage() {
+        document.getElementById("registrationForm").addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            const nombre = document.getElementById("nombre").value;
+            const apellido = document.getElementById("apellido").value;
+            const username = document.getElementById("username").value;
+            const fechaNacimiento = document.getElementById("fechaNacimiento").value;
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+
+            const userData = {
+                nombre: nombre,
+                apellido: apellido,
+                username: username,
+                fechaNacimiento: fechaNacimiento,
+                email: email,
+                password: password
+            };
+
+            document.getElementById("registrationForm").reset();
+            let usernameNoExistente = true;
+            let emailNoExistente = true;
+            for(let usuario of usuarios){
+                if(usuario.username == userData.username){
+                    usernameNoExistente = false;
+                    alert(`El nombre de usuario ${userData.username} ya esta tomado`);
+                    break;
+                }
+                if(usuario.email == userData.email){
+                    emailNoExistente = false;
+                    alert(`El email ${userData.email} ya esta en uso`);
+                    break;
+                }
+            }
+            if(usernameNoExistente && emailNoExistente ){
+                usuarios.push(userData);
+                alert(`    ¡Bienvenido ${userData.username}!
+                    Su usuario fue creado exitosamente`);
+            }
+        });
+    }
+    const currentPage = window.location.pathname;
+    if (currentPage.includes("/html/InicioSesion.html")) {
+        initializeLoginPage();
+    } else if (currentPage.includes("/html/Registro.html")) {
+        initializeRegistrationPage();
+    }
+});
+
 
